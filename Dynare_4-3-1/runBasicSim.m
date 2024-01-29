23b5095bf84ba7aff4834df8ca045d8f3d00776f
Krzysztof Bankowski <krzysztof.bankowski@ecb.europa.eu>
2024-01-29 11:10:08 +0100

    Running simulation 3 with productivity shocks

--- Dynare_4-3-1/runBasicSim.m
+++ Dynare_4-3-1/runBasicSim.m
@@ -31,3 +31,6 @@ dynare shock_eab_gy1.mod
 % shock simulation: 4-period g shock in EAB region (with some nuanced shock
 % values)
 dynare shock_eab_gy2.mod
+
+% shock simulation: productivity shock
+dynare shock_eab_tech.mod
