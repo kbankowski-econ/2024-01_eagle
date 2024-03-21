d3d7d8f06aeb281049d035b9577641009b1935bf
bankows <kristofer.bank@gmail.com>
2024-03-21 15:10:35 +0100

    Temporary folder to check parsing

---
+++ eagleParsingTemp/runModelParsing.m
@@ -0,0 +1,58 @@
+%% preamble
+clear all; close all; clc; restoredefaultpath
+% Add some paths
+currentFolder = pwd; % Get current directory
+[parentFolder, ~, ~] = fileparts(currentFolder);
+addpath(genpath(parentFolder));
+
+%% performing data transformation
+fprintf('\n\n\n+++ Initialising the project ...\n');
+% calling a specific user input not tracked by GIT
+utils.call.paths;
+% Call Iris
+addpath(iris_path);
+iris.startup
+% Call Dynare
+addpath(dynare_6_0);
+dynare_config
+
+%% --------------------
+%  Setting up  options
+%  --------------------
+options_ecb.mod_run = 'nostrict';
+options_ecb.mod_path = sprintf('-I%s/%s/submodules', project_path, 'eagleParsing');
+options_ecb.simulation = 1; % Do we want to simulate long-run model yes=1 and no=0 
+options_ecb.normalization = 1; % Do we want to normalize dataset yes=1 and no=0 
+norm_years = 200; % Normalization horizon #of_years
+options_dynare.solve_algo = 0; % Define algorithm to be used by Dynare
+options_dynare.solve_tolf = 1e-5; % Define precision of the used solver
+options.runParsing = true;
+
+
+%% ----------------------
+%  Parsing all mod files
+%  ----------------------
+cd(fullfile(project_path, 'eagleParsing','modFiles'));
+
+fprintf('Parsing %s ...\n', "eagleModel")
+
+try % To avoid json issue (here we absolutely need the json option)
+    dynare(sprintf('eagleModel'), options_ecb.mod_run, options_ecb.mod_path, 'nopreprocessoroutput');
+catch
+    dynare(sprintf('eagleModel'), options_ecb.mod_run, options_ecb.mod_path, 'nopreprocessoroutput');
+end
+endo =  M_.endo_names(~contains(M_.endo_names, 'AUX_'));
+endoProblematic = {...
+    'EAAEAB_imc'
+    'RWUS_imc'
+    'EABRW_imc'
+    'USEAA_imc'
+    'EAAEAB_imi' 
+    'EABRW_imi' 
+    'RWUS_imi'
+    'USEAA_imi'
+    };
+
+endoAdjusted = setdiff(endo, endoProblematic);
+% Creating .inc files which contain all informations on the model
+utils.ParseModel(sprintf('eagleModel'), sprintf('Parsed_eagleModel'), endoAdjusted, false)
---
+++ eagleParsingTemp/submodules/init.mod
@@ -0,0 +1,12 @@
+@#define countries = [ "EAA", "EAB", "RW", "US" ]
+@#define ea = "EA"
+//
+@#define asymmetric_trade_var = 0
+// Is sigma equal to one for any country ?
+@#define sigma_equals_one = [ 1, 1, 1, 1 ]
+// Is omega equal to zero for any country ?
+@#define omega_equals_zero = [ 0, 0, 0, 0 ]
+
+@#define countries2 = [ countries[2], countries[3], countries[4], countries[1] ]
+@#define countries3 = [ countries[3], countries[4], countries[1], countries[2] ]
+@#define countries4 = [ countries[4], countries[1], countries[2], countries[3] ]
