ac884d657392953d757f251d6bdf7a147dfe4dab
bankows <kristofer.bank@gmail.com>
2024-04-04 18:57:51 +0200

    steady1 (SUCCESSFUL run)

--- eagleParsingTemp/modFiles/+steady1/+sparse/+block/dynamic_131.m
+++ eagleParsingTemp/modFiles/+steady1/+sparse/+block/dynamic_131.m
@@ -1,6 +1,8 @@
 function [y, T] = dynamic_131(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
   y(3414)=y(3343);
-  y(2428)=y(2422)*y(2431)/y(225);
+  y(2424)=y(2423)*y(2418)/y(2130);
   y(3272)=((params(212)+T(3)-1)*y(3414)-params(212)*x(90)*y(3410))/(y(3410)*(1-x(90)));
+  y(2427)=y(2423)*y(2430)/y(2127);
+  y(2428)=y(2422)*y(2431)/y(225);
   y(2627)=y(2603)/(y(3504)*y(2615));
 end
--- eagleParsingTemp/modFiles/+steady1/+sparse/+block/static_198.m
+++ eagleParsingTemp/modFiles/+steady1/+sparse/+block/static_198.m
@@ -1,6 +1,6 @@
 function [y, T, residual, g1] = static_198(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
 residual=NaN(1, 1);
-  T(1177)=params(320)*T(664)^params(325);
+  T(1177)=params(320)*T(663)^params(325);
   residual(1)=(y(1450))-((1-params(320))*(y(1481)/y(1480))^(-params(325))+y(1450)*T(1177));
 if nargout > 3
     g1_v = NaN(1, 1);
