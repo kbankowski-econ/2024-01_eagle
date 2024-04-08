54fcd6e208577b06dad3739f2047b7562efa64a2
bankows <kristofer.bank@gmail.com>
2024-04-08 13:31:03 +0200

    Steady0 (NOT successful)

--- eagleParsingTemp/runBasicSim.m
+++ eagleParsingTemp/runBasicSim.m
@@ -25,6 +25,11 @@ dynare_config
 cd(fullfile(project_path, 'eagleParsingTemp','modFiles'));
 dynare('load0.mod', sprintf('-I%s/%s/submodules', project_path, 'eagleParsingTemp'), 'savemacro');
 
+varList = load0output.M_.endo_names(~startsWith(load0output.M_.endo_names, 'AUX_ENDO_'));
+for aVar = string(reshape(varList, 1, []))
+    load0struct.ssValues.(aVar) = load0output.oo_.steady_state(strcmp(aVar, varList));
+end
+
 %% non-SS version of the model
 dynare('eagleModel.mod', sprintf('-I%s/%s/submodules', project_path, 'eagleParsingTemp'), 'savemacro');
 
