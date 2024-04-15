d61c578517597b8403478b5f029c25a0d2c7b617
bankows <kristofer.bank@gmail.com>
2024-04-15 23:31:33 +0200

    perfect_foresight_solver(stack_solve_algo=5) (Mac, workes very slow)

--- eagleParsingTemp/modFiles/+shock_eab_gy1/+sparse/+block/dynamic_100.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = dynamic_100(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(2975)=y(2997)*log(y(5134)-params(28)*y(2495))-1/(1+params(76))*y(2899)^(1+params(76))+params(9)*y(5614);
-end
