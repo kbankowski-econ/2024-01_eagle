214424af85ffbef254afaa87a3240530f4e4006e
bankows <kristofer.bank@gmail.com>
2024-10-18 15:21:44 +0200

    Preambles in the drivers

--- runPlots.m
+++ runPlots.m
@@ -1,20 +1,6 @@
 %% preamble
-clear all; close all; clc; restoredefaultpath
-% Add some paths
-currentFolder = pwd; % Get current directory
-[parentFolder, ~, ~] = fileparts(currentFolder);
-addpath(genpath(parentFolder));
-
-%% performing data transformation
-fprintf('\n\n\n+++ Initialising the project ...\n');
-% calling a specific user input not tracked by GIT
+clear all; close all; clc; 
 utils.call.paths;
-% Call Iris
-addpath(iris_path);
-iris.startup
-% Call Dynare
-addpath(dynare_6_0);
-dynare_config
 
 %% ----------------
 % Loading the databases
