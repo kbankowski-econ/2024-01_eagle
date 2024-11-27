ee3f2ac3134d4b018c9c193e51f5e9b00607ec60
bankows <kristofer.bank@gmail.com>
2024-11-27 16:08:54 +0100

    Loading with Dynare 6.0 (none of the versions tried, 5.5, 6.0, 6.1, 6.2, 7 preliminary changes anything; agreed with Emile to use 6.1)

--- eagleParsingTemp/runBasicSim.m
+++ eagleParsingTemp/runBasicSim.m
@@ -18,7 +18,7 @@ utils.call.paths;
 addpath(iris_path);
 iris.startup
 % Call Dynare
-addpath(dynare_6_1_official, '-frozen');
+addpath(dynare_7_2024_04_12_1835, '-frozen');
 dynare_config
 
 %% SS version of the model
