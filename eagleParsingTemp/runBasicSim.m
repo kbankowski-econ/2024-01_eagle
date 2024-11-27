711ab615aaa6eb43d725bcf060686c30df539787
bankows <kristofer.bank@gmail.com>
2024-11-27 16:21:48 +0100

    steady4 [success 4.30 min]

--- eagleParsingTemp/runBasicSim.m
+++ eagleParsingTemp/runBasicSim.m
@@ -18,7 +18,7 @@ utils.call.paths;
 addpath(iris_path);
 iris.startup
 % Call Dynare
-addpath(dynare_7_2024_04_12_1835, '-frozen');
+addpath(dynare_6_1_official, '-frozen');
 dynare_config
 
 %% SS version of the model
