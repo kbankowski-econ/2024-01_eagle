9ab47698bad9d37941dd94da193e34e0b4a81c70
bankows <kristofer.bank@gmail.com>
2024-12-11 20:55:43 +0100

    [even old values fail] something is grossly wrong

--- eagleParsingTemp/runHomothopyValuesModify.m
+++ eagleParsingTemp/runHomothopyValuesModify.m
@@ -3,7 +3,7 @@ load(fullfile(project_path, "eagleParsingTemp/modFiles/steady2/Output/steady2_re
 aaa = load(fullfile(project_path, "eagleParsingTemp/modFiles/steady9/Output/steady9_results.mat"));
 options_.homotopy_values = aaa.options_.homotopy_values;
 
-fid = fopen('trade_matrix_values_calibrated_my.mod', 'r');
+fid = fopen('trade_matrix_values_calibrated_old.mod', 'r');
 
 % Initialize an empty vector to store the values
 values = [];
