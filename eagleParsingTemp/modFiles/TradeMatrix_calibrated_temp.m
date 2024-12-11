efca6e9751c352cb4bdc11b6429d4cc4415b7ee8
bankows <kristofer.bank@gmail.com>
2024-12-11 11:19:38 +0100

    New function for trade matrix mod file

--- eagleParsingTemp/modFiles/TradeMatrix_calibrated_temp.m
+++ eagleParsingTemp/modFiles/TradeMatrix_calibrated_temp.m
@@ -1,58 +1,8 @@
-
 % Define country array
 countries = ["EAA", "EAB", "EAC", "EAD", "EAE", "EAF", "EAG", "EAH", "EAI", "EAJ", "EAK", "EAL", "EAM", "RW", "US"];
-
-    xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
-    table.imcy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B44:P58');
-    table.imcy.Properties.RowNames = countries;
-    table.imcy.Properties.VariableNames = countries;
-
-    xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
-    table.imiy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B62:P76');
-    table.imiy.Properties.RowNames = countries;
-    table.imiy.Properties.VariableNames = countries;
-
-    table.imcy("EAF", :) = table.imcy("EAF", :)./2;
-    table.imiy("EAF", :) = table.imiy("EAF", :)./2;
-
-    table.imcgy = table.imcy.*0.1;
-    table.imcy = table.imcy.*0.9;
-
-    table.imigy = table.imiy.*0.1;
-    table.imiy = table.imiy.*0.9;
-
-
 % Create circular array for residual countries
 shiftAmount = 13;  % Making the shift amount explicit as a variable
 countriesAux = [countries, countries];  % Double array for circular indexing
-
-% Open file for writing
-fileID = fopen('country_labels.txt', 'w');
-
-% Item loop
-for aItem = ["imcy", "imcgy", "imiy", "imigy"]
-    % Outer loop
-    for i = 1:length(countries)
-        aCtry1 = countries(i);
-        % Get residual country with circular indexing
-        aCtryResid = countriesAux(i + shiftAmount);
-        
-        % Inner loop excluding both current and residual countries
-        validCountries = countries ~= aCtry1 & countries ~= aCtryResid;
-        for aCtry2 = countries(validCountries)
-            fprintf(fileID, '%s%s_%s, %f;\n', aCtry1, aCtry2, aItem, table.(aItem){aCtry1, aCtry2});
-        end
-    end
-end
-
-% Item loop
-    % Outer loop
-    for i = 1:length(countries)
-        aCtry1 = countries(i);
-for aItem = ["imcy", "imcgy", "imiy", "imigy"]
-        fprintf(fileID, '%s_%s, %f;\n', aCtry1, aItem, sum(table.(aItem){aCtry1, :}, 'omitnan'));
-    end
-    end
 
 sizeStruct = struct(...
     'EAA', 0.012663, ...
@@ -71,12 +21,106 @@ sizeStruct = struct(...
     'RW', 0.470000, ...
     'US', 0.310000);    
 
-% Item loop
-    % Outer loop
+
+%% Creating orignal trade tables
+fid = fopen('trade_matrix_values_calibrated_old.mod', 'r');
+data = textscan(fileID, '%s %f', 'Delimiter', ',');
+fclose(fileID);
+
+% Create the structure
+for i = 1:2:length(data{1})  % Step by 2 to skip semicolons
+    % Remove any whitespace from the field name
+    fieldName = strtrim(data{1}{i});
+    % Get the value and remove the semicolon
+    value = data{2}(i);
+    % Store in structure
+    s.(fieldName) = value;
+end
+
+% alocate the values from the structure
+for aItem = ["imcy", "imcgy", "imiy", "imigy"]
+    tableOrig.(aItem) = array2table(nan(length(countries), length(countries)), 'RowNames', countries, 'VariableNames', countries);
     for i = 1:length(countries)
         aCtry1 = countries(i);
-        fprintf(fileID, '%s_size, %f;\n', aCtry1, sizeStruct.(aCtry1));
+        % Get residual country with circular indexing
+        aCtryResid = countriesAux(i + shiftAmount);
+        for aCtry2 = countries
+            aField = aCtry1+aCtry2+"_"+aItem;
+            if isfield(s, aField)
+                tableOrig.imcy{aCtry1, aCtry2} = s.(aCtry1+aCtry2+"_"+aItem);
+            end
+        end
+        tableOrig.imcy{aCtry1, aCtryResid} = s.(aCtry1+"_"+aItem) - sum(tableOrig.imcy{aCtry1, :}, 'omitnan');
     end
+end
 
-% Close the file
-fclose(fileID);
+
+%% Creating new tables
+    xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
+    newTable.imcy = table();
+    newTable.imcy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B44:P58');
+    newTable.imcy.Properties.RowNames = countries;
+    newTable.imcy.Properties.VariableNames = countries;
+
+    newTable.imiy = table();
+    xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
+    newTable.imiy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B62:P76');
+    newTable.imiy.Properties.RowNames = countries;
+    newTable.imiy.Properties.VariableNames = countries;
+
+    newTable.imcy("EAF", :) = newTable.imcy("EAF", :)./2;
+    newTable.imiy("EAF", :) = newTable.imiy("EAF", :)./2;
+
+    newTable.imcgy = table();
+    newTable.imcgy = newTable.imcy.*0.1;
+    newTable.imcy = newTable.imcy.*0.9;
+
+    newTable.imigy = table();
+    newTable.imigy = newTable.imiy.*0.1;
+    newTable.imiy = newTable.imiy.*0.9;
+
+
+%% writing mod trade calibration file
+writeTradeModFile('trade_matrix_values_calibrated_new.mod', newTable, sizeStruct, countries, countriesAux, shiftAmount)
+
+%% local functions
+function writeTradeModFile(aFileName, aTable, sizeStruct, countries, countriesAux, shiftAmount)
+
+    fileID = fopen(aFileName, 'w');
+    
+    % Item loop
+    for aItem = ["imcy", "imcgy", "imiy", "imigy"]
+        % Outer loop
+        for i = 1:length(countries)
+            aCtry1 = countries(i);
+            % Get residual country with circular indexing
+            aCtryResid = countriesAux(i + shiftAmount);
+            
+            % Inner loop excluding both current and residual countries
+            validCountries = countries ~= aCtry1 & countries ~= aCtryResid;
+            for aCtry2 = countries(validCountries)
+                fprintf(fileID, '%s%s_%s, %f;\n', aCtry1, aCtry2, aItem, aTable.(aItem){aCtry1, aCtry2});
+            end
+        end
+    end
+    
+    % Item loop
+        % Outer loop
+        for i = 1:length(countries)
+            aCtry1 = countries(i);
+    for aItem = ["imcy", "imcgy", "imiy", "imigy"]
+            fprintf(fileID, '%s_%s, %f;\n', aCtry1, aItem, sum(aTable.(aItem){aCtry1, :}, 'omitnan'));
+        end
+        end
+    
+    % Item loop
+        % Outer loop
+        for i = 1:length(countries)
+            aCtry1 = countries(i);
+            fprintf(fileID, '%s_size, %f;\n', aCtry1, sizeStruct.(aCtry1));
+        end
+    
+    % Close the file
+    fclose(fileID);
+
+end
