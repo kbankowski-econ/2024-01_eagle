93a5d03a63df329c46cdfc4c93c82971123850f1
emile.petraviciute@ecb.europa.eu <emile.petraviciute@ecb.europa.eu>
2025-01-14 16:17:51 +0100

    Updating trade matrix by country. ss works for all europe countries, fails for both RW and US

--- eagleParsingTemp/modFiles/TradeMatrix_step_by_country.m
+++ eagleParsingTemp/modFiles/TradeMatrix_step_by_country.m
@@ -1,5 +1,5 @@
 % Read the original file
-fid = fopen('trade_matrix_values_calibrated_int.mod', 'r');
+fid = fopen('trade_matrix_values_calibrated_int_EAL.mod', 'r');
 original_content = textscan(fid, '%s', 'Delimiter', '\n');
 original_content = original_content{1};
 fclose(fid);
@@ -10,15 +10,15 @@ new_content = textscan(fid, '%s', 'Delimiter', '\n');
 new_content = new_content{1};
 fclose(fid);
 
-% Create a map of new EAI values
-new_EAI_values = containers.Map('KeyType', 'char', 'ValueType', 'char');
+% Create a map of new US values
+new_US_values = containers.Map('KeyType', 'char', 'ValueType', 'char');
 for i = 1:length(new_content)
     line = new_content{i};
-    if startsWith(line, 'EAI')
+    if startsWith(line, 'US')
         parts = strsplit(line, ',');
         key = strtrim(parts{1});
         value = strtrim(parts{2});
-        new_EAI_values(key) = value;
+        new_US_values(key) = value;
     end
 end
 
@@ -28,15 +28,15 @@ for i = 1:length(original_content)
     line = original_content{i};
     parts = strsplit(line, ',');
     key = strtrim(parts{1});
-    if isKey(new_EAI_values, key)
-        updated_content{i} = sprintf('%s, %s', key, new_EAI_values(key));
+    if isKey(new_US_values, key)
+        updated_content{i} = sprintf('%s, %s', key, new_US_values(key));
     else
         updated_content{i} = line;
     end
 end
 
 % Write the updated content back to a new file
-fid = fopen('trade_matrix_values_calibrated_int_EAI.mod', 'w');
+fid = fopen('trade_matrix_values_calibrated_int_US.mod', 'w');
 fprintf(fid, '%s\n', updated_content{:});
 fclose(fid);
 
