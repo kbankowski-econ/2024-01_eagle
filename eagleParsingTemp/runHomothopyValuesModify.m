65897aa799c724fb11a92abcc0b0513bbcef8d99
bankows <kristofer.bank@gmail.com>
2025-01-21 11:00:37 +0100

    RW calib (step1)

--- eagleParsingTemp/runHomothopyValuesModify.m
+++ eagleParsingTemp/runHomothopyValuesModify.m
@@ -52,10 +52,10 @@ countries = ["EAA", "EAB", "EAC", "EAD", "EAE", "EAF", "EAG", "EAH", "EAI", "EAJ
 shiftAmount = 12;  % Making the shift amount explicit as a variable
 countriesAux = [countries, countries];  % Double array for circular indexing
 
-rangeMatrix = "Q"+["3", "21", "39", "57", "93"];
+rangeMatrix = "A"+["3", "21", "39", "57", "93"];
 rangeStruct = struct('imcy', rangeMatrix(1), 'imcgy', rangeMatrix(2), 'imiy', rangeMatrix(3), 'imigy', rangeMatrix(4), 'rer', rangeMatrix(5));
 
-rangeFlatMatrix = "Q"+["110", "114", "122", "127"];
+rangeFlatMatrix = "A"+["110", "114", "122", "127"];
 rangeFlatStruct = struct('y', rangeFlatMatrix(1), 'py', rangeFlatMatrix(2), 'size', rangeFlatMatrix(3), 'pex', rangeFlatMatrix(4));
 
 
@@ -77,7 +77,7 @@ for aItem = ["imcy", "imcgy", "imiy", "imigy", "rer"]
             tableForXls.(aItem){"Total", aCtry1} = paramStruct.(aCtry1+"_"+aItem);
         end
     end
-    writetable(tableForXls.(aItem), xlsFilePath, 'Sheet', 'EAL-recompWithUS', 'Range', rangeStruct.(aItem), 'WriteRowNames', true, 'WriteVariableNames', true);
+    writetable(tableForXls.(aItem), xlsFilePath, 'Sheet', 'calibRW2', 'Range', rangeStruct.(aItem), 'WriteRowNames', true, 'WriteVariableNames', true);
 end
 
 
@@ -91,7 +91,7 @@ for aItem = ["y", "py", "size", "pex"]
             tableFlatForXls.(aItem){"Row", aCtry1} = ssStruct.(aCtry1+"_"+aItem);
         end
     end
-    writetable(tableFlatForXls.(aItem), xlsFilePath, 'Sheet', 'EAL-recompWithUS', 'Range', rangeFlatStruct.(aItem), 'WriteRowNames', true, 'WriteVariableNames', true);
+    writetable(tableFlatForXls.(aItem), xlsFilePath, 'Sheet', 'calibRW2', 'Range', rangeFlatStruct.(aItem), 'WriteRowNames', true, 'WriteVariableNames', true);
 end
 
 %%
