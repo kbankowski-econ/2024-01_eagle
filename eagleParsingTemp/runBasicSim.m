8429af6dead9266622fe2770969c70b0b2c73629
bankows <kristofer.bank@gmail.com>
2025-01-29 19:10:14 +0100

    Preparing stand-alone file for Janos (file with 2.5% rebalancing for Germany-US, surprisingly it works)

--- eagleParsingTemp/runBasicSim.m
+++ eagleParsingTemp/runBasicSim.m
@@ -74,6 +74,7 @@ copyfile(...
 dynare('steady2bMacroexp.mod');
 dynare('steady2bMacroexp1.mod');
 dynare('steady2bMacroexp2.mod');
+dynare('steady2bMacroexp3.mod');
 
 %%
 dynare('steady2.mod', sprintf('-I%s/%s/submodules', project_path, 'eagleParsingTemp'), 'savemacro');
