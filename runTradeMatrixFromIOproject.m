96182444be5a644574c3633189f987e6e356c55c
emile.petraviciute@ecb.europa.eu <emile.petraviciute@ecb.europa.eu>
2025-04-03 13:15:52 +0200

    Reading tby from a csv file instead of manually including in the code

--- runTradeMatrixFromIOproject.m
+++ runTradeMatrixFromIOproject.m
@@ -1,10 +1,6 @@
 % Define country array
 countries = [ "RA", "AT", "BE", "ES", "FI", "FR", "GR", "IT", "NL", "PT", "DE", "RU", "RW", "US"];
-calibCtryListModNames = ["EAB", "EAC", "EAD", "EAE", "EAG", "EAH", "EAI", "EAJ", "EAK", "EAL", "EAM"];
-calibCtryListStdNames = ["AT", "BE", "FI", "FR", "NL", "ES", "GR", "IE", "IT", "PT", "DE"];
 importItemListModNames = ["imcy", "imcgy", "imiy", "imigy"];
-demandItemListModNames = ["cy", "cgy", "iy", "igy"];
-demandItemListIoNames = ["FinalConHH", "FinalConGov", "PrivateInv", "GovInv"];
 
 % Create circular array for residual countries
 shiftAmount = 12;  % Making the shift amount explicit as a variable
@@ -19,61 +15,14 @@ sizeTable = readtable(csvFileName, 'ReadRowNames', true, 'Range', 'A1:b15');
 sizeStruct = table2struct(sizeTable);
 sizeStruct = cell2struct(struct2cell(sizeStruct)', sizeTable.Properties.RowNames);
 
-
-% %% Creating orignal trade tables
-% fid = fopen('trade_matrix_values_calibrated_old.mod', 'r');
-% data = textscan(fid, '%s %f', 'Delimiter', ',');
-% fclose(fid);
-% 
-% % Create the structure
-% for i = 1:2:length(data{1})  % Step by 2 to skip semicolons
-%     % Remove any whitespace from the field name
-%     fieldName = strtrim(data{1}{i});
-%     % Get the value and remove the semicolon
-%     value = data{2}(i);
-%     % Store in structure
-%     s.(fieldName) = value;
-% end
-% 
-% % alocate the values from the structure
-% for aItem = ["imcy", "imcgy", "imiy", "imigy"]
-%     origTable.(aItem) = array2table(nan(length(countries), length(countries)), 'RowNames', countries, 'VariableNames', countries);
-%     for i = 1:length(countries)
-%         aCtry1 = countries(i);
-%         % Get residual country with circular indexing
-%         aCtryResid = countriesAux(i + shiftAmount);
-%         validCountries = countries ~= aCtry1 & countries ~= aCtryResid;
-%         for aCtry2 = countries(validCountries)
-%                 origTable.(aItem){aCtry1, aCtry2} = s.(aCtry1+aCtry2+"_"+aItem);
-%         end
-%         origTable.(aItem){aCtry1, aCtryResid} = s.(aCtry1+"_"+aItem) - sum(origTable.(aItem){aCtry1, :}, 'omitnan');
-%     end
-% end
-% 
-% 
-% %% Creating new tables (based on the ECHT tool)
-%     xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
-%     newTable.imcy = table();
-%     newTable.imcy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B44:P58');
-%     newTable.imcy.Properties.RowNames = countries;
-%     newTable.imcy.Properties.VariableNames = countries;
-% 
-%     newTable.imiy = table();
-%     xlsFileName = fullfile(project_path_echt, "import_shares_test.xlsx");
-%     newTable.imiy = readtable(xlsFileName, 'Sheet', 'Sheet1', 'Range', 'B62:P76');
-%     newTable.imiy.Properties.RowNames = countries;
-%     newTable.imiy.Properties.VariableNames = countries;
-% 
-%     newTable.imcy("EAF", :) = newTable.imcy("EAF", :)./2;
-%     newTable.imiy("EAF", :) = newTable.imiy("EAF", :)./2;
-% 
-%     newTable.imcgy = table();
-%     newTable.imcgy = newTable.imcy.*0.1;
-%     newTable.imcy = newTable.imcy.*0.9;
-% 
-%     newTable.imigy = table();
-%     newTable.imigy = newTable.imiy.*0.1;
-%     newTable.imiy = newTable.imiy.*0.9;
+% loading the tby structure, but first as a table
+tbyStruct = struct();
+csvFileName = fullfile(project_path_io, "databases/tables/oecd/eu", "tby.csv");
+tbyTable = table();
+tbyTable = readtable(csvFileName, 'ReadRowNames', true, 'Range', 'A1:b15');
+% Convert table to structure
+tbyStruct = table2struct(tbyTable);
+tbyStruct = cell2struct(struct2cell(tbyStruct)', tbyTable.Properties.RowNames);
 
 %% Creating new tables (based on the IO tool)
 
@@ -84,45 +33,10 @@ for aItem = ["imcy", "imiy", "imcgy", "imigy"]
     newTable.(aItem) = readtable(csvFileName, 'ReadRowNames', true, 'Range', 'A1:o15');
 end
 
-% %% Creating new tables (based on the import content; this ammends the old table with the new information)
-% 
-% % starting with the orignal table
-% myTable = origTable;
-% 
-% % importing import content values from the i-o project
-% load(fullfile(project_path_io, "/databases/CalcDb_importWeights.mat"), 'WeightsDb');
-% 
-% % creating a structure with import contant
-% importContentStruct = struct();
-% for aCtryModName = calibCtryListModNames(1:11)
-%     for aDemandItem = demandItemListModNames
-%         importContentStruct.(aCtryModName).(aDemandItem) = ...
-%             mean(WeightsDb.(calibCtryListStdNames(aCtryModName == calibCtryListModNames)).(demandItemListIoNames(aDemandItem == demandItemListModNames)).data, 'omitnan');
-%     end
-% end
-% 
-% % creating a structure with steady1 ss values
-% steady1struct = load(fullfile(project_path, "eagleParsingTemp/modFiles/steady1/Output/steady1_results.mat"));
-% steady1struct.myStruct = cell2struct(num2cell(steady1struct.oo_.steady_state), steady1struct.M_.endo_names, 1);
-% 
-% % updating the myTables
-% for aCtryModName = calibCtryListModNames(1:11)
-%     for aItem = importItemListModNames
-%         aItemDemand = demandItemListModNames(aItem == importItemListModNames);
-%         myTable.(aItem){aCtryModName, :} = ...
-%             origTable.(aItem){aCtryModName, :}/sum(newTable.(aItem){aCtryModName, :}, "omitmissing") ...
-%             *importContentStruct.(aCtryModName).(aItemDemand)*steady1struct.myStruct.(aCtryModName + "_" + aItemDemand);
-%     end
-% end
-% 
-
 %% writing mod trade calibration file
-writeTradeModFile(fullfile(project_path, 'eagleParsingTemp/modFiles', 'trade_matrix_values_calibrated_new.mod'), newTable, sizeStruct, countries, countriesAux, shiftAmount)
-% writeTradeModFile('trade_matrix_values_calibrated_oldReprinted.mod', origTable, sizeStruct, countries, countriesAux, shiftAmount)
-% writeTradeModFile('trade_matrix_values_calibrated_EABEAM.mod', myTable, sizeStruct, countries, countriesAux, shiftAmount)
-
+writeTradeModFile(fullfile(project_path, 'eagleParsingTemp/modFiles', 'trade_matrix_values_calibrated_new.mod'), newTable, sizeStruct, tbyStruct, countries, countriesAux, shiftAmount)
 %% local functions
-function writeTradeModFile(aFileName, aTable, sizeStruct, countries, countriesAux, shiftAmount)
+function writeTradeModFile(aFileName, aTable, sizeStruct, tbyStruct, countries, countriesAux, shiftAmount)
     fileID = fopen(aFileName, 'w');
     % Item loop
     for aItem = ["imcy", "imcgy", "imiy", "imigy"]
@@ -154,23 +68,15 @@ function writeTradeModFile(aFileName, aTable, sizeStruct, countries, countriesAu
         aCtry1 = countries(i);
         fprintf(fileID, '%s_size, %f;\n', aCtry1, sizeStruct.(aCtry1));
     end
+
+    % Outer loop for printing tby, excluding 'US'
+    for i = 1:length(countries)
+        aCtry1 = countries(i);
+        if ~strcmp(aCtry1, 'US')  % Check if the country is not 'US'
+            fprintf(fileID, '%s_tby, %f;\n', aCtry1, tbyStruct.(aCtry1));
+    end
+end
     
-    % Append the additional text at the end of the file
-    % //TODO: to be delted at some point once we have a more elegant way to
-    % do it
-    fprintf(fileID, 'RA_tby, 0.295845481096323;\n');
-    fprintf(fileID, 'AT_tby, 0.0590048273247692;\n');
-    fprintf(fileID, 'BE_tby, 0.097760354486184;\n');
-    fprintf(fileID, 'ES_tby, 0.0149158507329361;\n');
-    fprintf(fileID, 'FI_tby, 0.0523095729115864;\n');
-    fprintf(fileID, 'FR_tby, 0.0024733397043501;\n');
-    fprintf(fileID, 'GR_tby, -0.0466028705274942;\n');
-    fprintf(fileID, 'IT_tby, 0.0222376408197778;\n');
-    fprintf(fileID, 'NL_tby, 0.113012105819699;\n');
-    fprintf(fileID, 'PT_tby, -0.0242200598497534;\n');
-    fprintf(fileID, 'DE_tby, 0.0623161050500509;\n');
-    fprintf(fileID, 'RU_tby, 0.0594985182084918;\n');
-    fprintf(fileID, 'RW_tby, -0.00544586845861045;\n');
     fprintf(fileID, 'RA_nuc, 0.90;\n');
     fprintf(fileID, 'AT_nuc, 0.65;\n');
     fprintf(fileID, 'BE_nuc, 0.65;\n');
