938055e9f94df2989c531f4ed4abbfd2cf7ea4af
bankows <kristofer.bank@gmail.com>
2025-05-09 11:02:48 -0400

    Some progress with the tables for Section 2.2

--- docs/2025-02_working-paper/tables/monetaryFiscalPolicy.m
+++ docs/2025-02_working-paper/tables/monetaryFiscalPolicy.m
@@ -11,7 +11,7 @@ function monetaryFiscalPolicy(envi, aStruct, fileName)
     % Table header
     fprintf(fid, '\\begin{table}[htbp]\n');
     fprintf(fid, '    \\centering\n');
-    fprintf(fid, '    \\caption{Monetary and fiscal policy.}\n');
+    fprintf(fid, '    \\caption{Fiscal policy specification.}\n');
     fprintf(fid, '    \\label{tab:policy}\n');
     fprintf(fid, '    {\\small\n');
     fprintf(fid, '    \\begin{tabular}{>{\\raggedright\\arraybackslash}p{6.2cm}*{14}{>{\\centering\\arraybackslash}p{0.8cm}}}\n');
@@ -19,23 +19,19 @@ function monetaryFiscalPolicy(envi, aStruct, fileName)
     fprintf(fid, '     & %s \\\\\n', strjoin(envi.Meta.ctryList, ' & '));
     fprintf(fid, '    \\hline\n');
 
-    % Monetary authority section
-    fprintf(fid, '    \\multicolumn{15}{l}{\\textbf{Monetary authority}} \\\\\n');
-    fprintf(fid, utils.prepareTableLine(envi, 'Inflation target', '\Pi^{*}', "pi4target", aStruct.params));
-    fprintf(fid, utils.prepareTableLine(envi, 'Interest rate inertia', '\phi_{\text{R}}', "phirr", aStruct.params));
-    fprintf(fid, utils.prepareTableLine(envi, 'Interest rate sensitivity to inflation gap', '\phi_{\Pi}', "phirpi", aStruct.params));
-    fprintf(fid, utils.prepareTableLine(envi, 'Interest rate sensitivity to output growth', '\phi_{\text{gY}}', "phirgy", aStruct.params));
-
     % Fiscal authority section
-    fprintf(fid, '    \\multicolumn{15}{l}{\\textbf{Fiscal authority}} \\\\\n');
-    fprintf(fid, utils.prepareTableLine(envi, 'Government debt-to-output ratio', '\overline{B}_{\text{Y}}', "by", aStruct.ssValues));
-    fprintf(fid, utils.prepareTableLine(envi, 'Sensitivity of lump-sum taxes to debt-to-output ratio', '\phi_{\text{BL}}', "phitb", aStruct.params));
+    fprintf(fid, '    \\multicolumn{15}{l}{\\textbf{Expenditure}} \\\\\n');
+    fprintf(fid, utils.prepareTableLine(envi, 'Consumption tax rate', '\tau^{\text{C}}', "tauc", aStruct.ssValues));
+    fprintf(fid, '    \\multicolumn{15}{l}{\\textbf{Taxation}} \\\\\n');
     fprintf(fid, utils.prepareTableLine(envi, 'Consumption tax rate', '\tau^{\text{C}}', "tauc", aStruct.ssValues));
     fprintf(fid, utils.prepareTableLine(envi, 'Dividend tax rate', '\tau^{\text{D}}', "taud", aStruct.ssValues));
     fprintf(fid, utils.prepareTableLine(envi, 'Capital income tax rate', '\tau^{\text{K}}', "tauk", aStruct.ssValues));
     fprintf(fid, utils.prepareTableLine(envi, 'Labor income tax rate', '\tau^{\text{N}}', "taun", aStruct.ssValues));
     fprintf(fid, utils.prepareTableLine(envi, 'Rate of social security contribution by firms', '\tau^{\text{W}_{\text{f}}}', "tauwh", aStruct.ssValues));
     fprintf(fid, utils.prepareTableLine(envi, 'Rate of social security contribution by households', '\tau^{\text{W}_{\text{h}}}', "tauwf", aStruct.ssValues));
+    fprintf(fid, '    \\multicolumn{15}{l}{\\textbf{Fiscal rule}} \\\\\n');
+    fprintf(fid, utils.prepareTableLine(envi, 'Government debt-to-output ratio', '\overline{B}_{\text{Y}}', "by", aStruct.ssValues));
+    fprintf(fid, utils.prepareTableLine(envi, 'Sensitivity of lump-sum taxes to debt-to-output ratio', '\phi_{\text{BL}}', "phitb", aStruct.params));
 
     % Table footer
     fprintf(fid, '    \\hline\n');
--- runLatexCalibTables.m
+++ runLatexCalibTables.m
@@ -11,10 +11,12 @@ steady7struct = dynareFunc.retrieveSteadyState(modelResults);
 % Table 1
 ssNationalAccounts(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/ssNationalAccounts.tex'));
 
+% Table 2
+monetaryFiscalPolicy(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/monetaryFiscalPolicy.tex'));
+
 hhAndFirmBehavior(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/hhAndFirmBehavior.tex'));
 realNominalRigidities(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/realNominalRigidities.tex'));
 priceAndWageMarkups(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/priceAndWageMarkups.tex'));
-monetaryFiscalPolicy(envi, steady7struct, fullfile(project_path, 'docs/2025-02_working-paper/tables/monetaryFiscalPolicy.tex'));
 internationalLinkages(envi, steady7struct, "MC", "private consumption");
 internationalLinkages(envi, steady7struct, "MI", "private investment");
 internationalLinkages(envi, steady7struct, "MCG", "government consumption");
