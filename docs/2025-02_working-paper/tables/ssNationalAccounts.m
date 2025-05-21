36b105b654aaf4cde26334922744edbd7db5f670
bankows <kristofer.bank@gmail.com>
2025-05-21 19:21:14 +0200

    Let's keep the sizes of the tables as they are and rather modified the content

--- docs/2025-02_working-paper/tables/ssNationalAccounts.m
+++ docs/2025-02_working-paper/tables/ssNationalAccounts.m
@@ -9,7 +9,7 @@ function ssNationalAccounts(envi, aStruct, fileName)
     fprintf(fid, '    \\label{tab:national_accounts}\n');
     fprintf(fid, '    \\footnotesize\n');
     fprintf(fid, '    \\renewcommand{\\arraystretch}{1.2}\n');
-    fprintf(fid, '    \\begin{tabular}{>{\\raggedright}p{5.5cm}*{14}{>{\\centering\\arraybackslash}p{1cm}}}\n');
+    fprintf(fid, '    \\begin{tabular}{>{\\raggedright}p{5.5cm}*{14}{>{\\centering\\arraybackslash}p{0.8cm}}}\n');
     fprintf(fid, '        \\toprule\n');
     fprintf(fid, '         & %s \\\\\n', strjoin(envi.Meta.ctryList, ' & '));
     fprintf(fid, '        \\midrule\n');
@@ -26,7 +26,7 @@ function ssNationalAccounts(envi, aStruct, fileName)
     fprintf(fid, utils.prepareTableLine(envi.Meta.ctryList, 'Imports (total)', '\overline{imy}', "imy", aStruct.ssValues, ' & %.1f', @(x) x*100));
     fprintf(fid, utils.prepareTableLine(envi.Meta.ctryList, 'Exports (total)', '\overline{exy}', "exy", aStruct.ssValues, ' & %.1f', @(x) x*100));
     fprintf(fid, utils.prepareTableLine(envi.Meta.ctryList, 'Trade balance', '\overline{tby}', "tby", aStruct.ssValues, ' & %.1f', @(x) x*100));
-    fprintf(fid, utils.prepareTableLine(envi.Meta.ctryList, 'Net foreign assets', '\overline{B_Y^{*}}', "bfytarget", aStruct.params, ' & %.1f', @(x) x*100));
+    fprintf(fid, utils.prepareTableLine(envi.Meta.ctryList, 'Net foreign assets', '\overline{B_Y^{*}}', "bfytarget", aStruct.params, ' & %.0f', @(x) x*100));
 
     % Production section
     fprintf(fid, '        \\multicolumn{15}{l}{\\textbf{Production}} \\\\\n');
