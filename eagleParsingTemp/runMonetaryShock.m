22c090bd0cff6ccda8009989f34f3ee50baf631e
bankows <kristofer.bank@gmail.com>
2025-10-12 14:51:08 -0400

    Taking out functions for calculating contributions

--- eagleParsingTemp/runMonetaryShock.m
+++ eagleParsingTemp/runMonetaryShock.m
@@ -27,53 +27,8 @@ databank.toCSV( ...
     , "Decimals", 3 ...    
 );
 
-%%
-
-% global variable has to be declared so that it is passed on to
-% createContributions function
-global M_
-M_ = monetarySimOutput.M_;
-
-aItemList = ["EA_y", "EA_pic4"];
-allItemList = aItemList;
-
-for aItem = aItemList
-    [ ...
-        contributionSeries.total.(aItem) ...
-        , contributionSeries.contrib.(aItem) ...
-        , contributionSeries.lhs.(aItem) ...
-        ] = createContributions( ...
-            char(aItem) ...
-            , convertFunc.Series2Dseries(monetarySimStruct.endoValues) ...
-            , convertFunc.Series2Dseries(monetarySimStruct.ssValues) ...
-        );
-    allItemList = [allItemList, contributionSeries.contrib.(aItem).Comment];
-
-    % Dynare decomposition is always an absolute difference; for this
-    % reason we need this transformation with rescaling of
-    % contributions
-    % the explanation on the validity of re-scaling is here: investigations/contribution-rescaling.pdf
-    tempComment = contributionSeries.contrib.(aItem).Comment;
-    contributionSeries.total.(aItem) = monetarySimStruct.irfValues.(aItem);
-    contributionSeries.contrib.(aItem) = contributionSeries.contrib.(aItem)/sum(contributionSeries.contrib.(aItem), 2)*monetarySimStruct.irfValues.(aItem);
-    % we have to do it because it is overwritten and blank
-    contributionSeries.contrib.(aItem).Comment = tempComment;
-    
-end
-
-meta.allItemList = unique(allItemList);
-
-colorTable = table( ...
-    'Size',[numel(unique(allItemList)), 1] ...
-    , 'VariableTypes', {'cell'} ...
-    , 'VariableNames',{'colorIndex'} ...
-    , 'RowNames',unique(allItemList) ...
-    );
-colormapSaved = linspecer(numel(unique(allItemList)));
-for aItemIndex = 1:numel(unique(allItemList))
-    colorTable{aItemIndex, 1} = {colormapSaved(aItemIndex, :)};
-end
-contributionSeries.colorTable = colorTable;
+%% calculating contribution series
+contributionSeries = dynareFunc.calculateContributions(["EA_y", "EA_pic4"], monetarySimOutput.M_, monetarySimStruct);
 
 %% investigating interest rate reaction upon the request from Sandra
 plotting.WP.eaCntryDecomposition(contributionSeries);
