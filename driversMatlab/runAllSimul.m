d345a127b6e08a613570234031806738cb7cb848
bankows <kristofer.bank@gmail.com>
2025-11-08 11:09:34 -0500

    Fine-tuning plotting so that it works for NGEU and non-NGEU shocks

--- driversMatlab/runAllSimul.m
+++ driversMatlab/runAllSimul.m
@@ -4,6 +4,6 @@ utils.call.paths;
 envi = environment.setup();
 
 %% Running all simulations in a loop
-for aSimul = string(envi.shockDict.Properties.RowNames(5))'
+for aSimul = string(envi.shockDict.Properties.RowNames(1))'
     functions.runEAGLEsimul(aSimul, "skipSimulation", true);
 end
