612745d9c171150168009367f829111cde61ff34
bankows <kristofer.bank@gmail.com>
2025-11-09 16:09:25 -0500

    Re-running EU-wide gov. investment so that it lasts 3Y

--- driversMatlab/runAllSimul.m
+++ driversMatlab/runAllSimul.m
@@ -4,6 +4,6 @@ utils.call.paths;
 envi = environment.setup();
 
 %% Running all simulations in a loop
-for aSimul = string(envi.shockDict.Properties.RowNames(23))'
-    functions.runEAGLEsimul(aSimul, "skipSimulation", true);
+for aSimul = string(envi.shockDict.Properties.RowNames(3))'
+    functions.runEAGLEsimul(aSimul, "skipSimulation", false);
 end
