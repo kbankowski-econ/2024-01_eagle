b81d9befc42b61ac2182159bf707e6cb79065124
bankows <kristofer.bank@gmail.com>
2025-11-09 12:31:24 -0500

    Running the shock for IT so see the effects

--- driversMatlab/runAllSimul.m
+++ driversMatlab/runAllSimul.m
@@ -4,6 +4,6 @@ utils.call.paths;
 envi = environment.setup();
 
 %% Running all simulations in a loop
-for aSimul = string(envi.shockDict.Properties.RowNames)'
+for aSimul = string(envi.shockDict.Properties.RowNames(23))'
     functions.runEAGLEsimul(aSimul, "skipSimulation", false);
 end
